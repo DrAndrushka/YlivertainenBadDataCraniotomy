5c273d221eb38d2163dcf689ed624adaf3c2de9c
Andris Zaguzovs <andriszaguzovs@Andriss-MacBook-Pro.local>
2026-05-10 12:35:52 +0300

    created ULTIMATE COLUMN RENAMER

--- ylivertainen/app.py
+++ ylivertainen/app.py
@@ -7,11 +7,12 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 from ylivertainen._pathing import setup_repo_path
 root = setup_repo_path()
+
+import streamlit as st
+import pandas as pd
 #================================================================================
 #                         🎨 Overall Background Design 🎨
 #================================================================================
-import streamlit as st
-
 st.set_page_config(layout="wide")
 st.markdown("""
     <style>
@@ -44,9 +45,9 @@ st.markdown("""
 #=============================
 starting_title = "Ylivertainen Bad Data Craniotomy"
 starting_text = "This is The Module for awesome doctors such as yourself. Love the hustle, love the push towards Greatness"
-skip_title = st.checkbox(label="Show only important stuff", value=True, help="Show/Hide the starting title")
+happy_path = st.checkbox(label="⚡ YLIVERTAINEN HAPPY PATH ⚡", value=True)
     
-if not skip_title:
+if not happy_path:
     st.title(starting_title)
     st.write(starting_text)
 
@@ -57,8 +58,7 @@ from ylivertainen.cleaning import YlivertainenDataCleaningSurg, pre_merge_check
 
 st.header("🧹 DATA CLEANING 🧹")
 
-skip_to_UCR = st.checkbox(label="Skip To Ultimate Column Renamer", value=False)
-if not skip_to_UCR:
+if not happy_path:
     st.write("============")
     col1, col2, col3, col4 = st.columns([1,1,1,4])
     show_dfs = col1.checkbox(label="Show DataFrames", value=False)
@@ -68,16 +68,97 @@ if not skip_to_UCR:
         )
 
 
+#==============================
+#    Ultimate Column Renamer
+#==============================
+
+#====== HELPERS ======
+def pretty_text(text, align, color, weight, size) -> None:
+    text_align = "text-align:" + align
+    colorrr = "color:" + color
+    font_weight = "font-weight:" + weight
+    font_size = "font-size:" + size
+
+    st.markdown(
+        f"<div style='{text_align}; {colorrr}; {font_weight}; {font_size};'>{text}</div>",
+        unsafe_allow_html=True)
+
+def submit_rename():
+    new_col_name = st.session_state.rename_input.strip()
+    if new_col_name:
+        st.session_state.rename_log.insert(0, f"{new_col_name} --> {PREVIEW_DF.columns[st.session_state.current_idx]}")
+        st.session_state.current_idx += 1
+        st.session_state.rename_input = ""
+
+
+#===========
+#Example
+#===========
+#COLUMN_RENAME_MAP = {'nmpd_diag': ('NMP pamata diagnoze: G45.9 -1; I64 - 2; I63.9 - 3',),}
+#===========
+raw_dir = root / "ylivertainen" / "data" / "raw"
+csvs = sorted(raw_dir.glob("*.csv"))
+RAW_COLUMNS = list(dict.fromkeys([col for csv in csvs for col in pd.read_csv(csv, nrows=0).columns]))
+dfs_list = [pd.read_csv(csv, nrows=3).reindex(columns=RAW_COLUMNS) for csv in csvs]
+PREVIEW_DF = pd.concat(dfs_list, ignore_index=True)
+
+
+# ===== SESSION STATE INIT =====
+if "rename_log" not in st.session_state:
+    st.session_state.rename_log = []
+if "rename_input" not in st.session_state:
+    st.session_state.rename_input = ""
+if "current_idx" not in st.session_state:
+    st.session_state.current_idx = 0
+
+# ===== TEMPORARY VALUES (to delete afterwards) =====
+
+
+# ===== OUTPUT =====
+col1,col2,col3,col4 = st.columns([1,0.1,1,1.5])
+
+if st.session_state.current_idx < len(RAW_COLUMNS):
+    with col1:
+        new_col_name = st.text_input(label="", key="rename_input", placeholder="new_col_name", on_change=submit_rename)
+    with col3:
+        st.dataframe(PREVIEW_DF[[PREVIEW_DF.columns[st.session_state.current_idx]]].head(3))
+    with col4:
+        pretty_text("🏗️ AWESOME RENAME LOG 🏗️", "center", "white", "600", "30px")
+        st.write("")
+
+        for row in st.session_state.rename_log:
+            pretty_text(row, "center", "white", "400", "20px")
+
+
+else:
+    st.success("YLIVERTAINEN SUCCESS")
+    
+    pretty_text("🏗️ AWESOME RENAME LOG 🏗️", "center", "white", "600", "30px")
+    st.write("")
+
+    for row in st.session_state.rename_log:
+        pretty_text(row, "center", "white", "400", "20px")
 
 
 
+    #TODO: creates COLUMN_RENAME_MAP in the background
+    
+    #TODO: decide how to make it very fast to arrange the columns
+        # no need for mouse (can do with just keyboard)
+        # very fast and intuitive
+        # same idea like in the UCR but!!! it shows the column (just one like in UCR) 
+        # and I input the number where to place it relatively to the other columns that I have right now in the shown df 
+            # creates the DF in real time
+            
+            # TEXT BOX   --   COLUMN NAME      
+            # (below) DF.head(3) that updates with every input
 
+    #TODO: show df with all new names (.head(5))
+        # with the button to show/hide it
 
-
-
-
-
-
+    
+    
+    
 
 
 
